6cbb0a0ff0a0d737e5005dc15551a03da8d6caab
Binatik <sadxas999@mail.ru>
2023-06-09 20:03:18 +0300

    инициализация member с дополнительными полями при заходе на канал

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,23 @@ async function run() {
 
 	const chimera = new Bot(token)
 
-	chimera.command('info', async (ctx) => {
-		const id = ctx.msg.message_thread_id
-		await ctx.api.sendMessage(ctx.chat.id, 'Версия telegram-bot 0.1', { message_thread_id: id })
+	// chimera.command('info', async (ctx) => {
+	// 	await ctx.reply('I got your message!', { message_thread_id: ctx.msg.message_thread_id })
+	// })
+	chimera.on('message:new_chat_members', async (ctx) => {
+		const members: Array<any> = []
+		const newMember = {
+			id: ctx.msg.from.id,
+			name: ctx.msg.from.first_name,
+			roles: [],
+		}
+
+		const isIdMember = members.some((member) => member.id !== newMember.id)
+		console.log(isIdMember)
+		if (isIdMember) return
+
+		members.push(newMember)
+		console.log(members)
 	})
 
 	chimera.start()
